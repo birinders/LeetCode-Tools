72e756931da3b6da716f173fa0f10bb6c9faf63a
birinders <birindersinghji@gmail.com>
2023-09-07 00:39:40 +0530

    added functions

--- all.py
+++ all.py
@@ -6,8 +6,8 @@ from itertools import chain, combinations, permutations
 import operator
 from heapq import *
 
-from tools.tree_maker import *
-from tools.linked_list import *
+from tree_maker import *
+from linked_list import *
 from copy import copy, deepcopy
 import random
 from math import sqrt, isqrt, comb, perm, ceil, floor, gcd, lcm
--- linked_list.py
+++ linked_list.py
@@ -1,27 +1,44 @@
-class LinkedNode:
+from typing import Iterable
+
+
+class ListNode:
     def __init__(self, val=0, next=None, prev=None) -> None:
         self.val = val
         self.next = next
-        self.prev = prev
+        if prev:
+            self.prev = prev
 
 
-def buildLinkedList(values, doubly=False):
+def buildLinkedList(values: Iterable, doubly: bool = False, get_tail: bool = False):
+    """
+    values: A 1d iterable
+    doubly: bool- Set true if you want the ll to be doubly linked
+    get_tail: returns the tail of the ll in the tuple (head, tail)
+
+    Return None if "values" is empty.
+
+    """
     if not values:
-        return LinkedNode()
+        return None
 
-    head = LinkedNode(values[0])
+    head = ListNode(values[0])
     prev = head
     for val in values[1:]:
-        new_node = LinkedNode(val)
+        new_node = ListNode(val)
         if doubly:
             new_node.prev = prev
         prev.next = new_node
         prev = new_node
 
+    if get_tail:
+        return head, new_node
     return head
 
 
-def convert_to_doubly(head):
+def convert_to_doubly(head: ListNode) -> ListNode:
+    """
+    Returns the head of the linked list after linking it doubly
+    """
     prev = None
     h1 = head
     while h1:
@@ -32,7 +49,12 @@ def convert_to_doubly(head):
     return head
 
 
-def print_ll(head, sep=" -> "):
+def print_ll(head: ListNode, sep: str = " -> ") -> None:
+    """
+    Prints the linked list with the provided seperator. If no seperator is provided, " -> " is used by default.
+
+    Note: Head may be some other Linked List object as long as it uses the .next pointer for the next node
+    """
     default_sep = " -> "
 
     if hasattr(head, "prev") and sep == default_sep:
@@ -44,15 +66,78 @@ def print_ll(head, sep=" -> "):
         head = head.next
     str_ans = map(str, ans)
     print(sep.join(str_ans))
-    return ans
+    # return ans
 
 
-def reverse_ll(head):
+def get_ll_elems(head: ListNode) -> list[object]:
+    """
+    Returns a list of all the elements inside the Linked List.
+
+    Note: Head may be some other Linked List object as long as it uses the .next pointer for the next node
+    """
+    ret = []
+    while head:
+        ret.append(head.val)
+        head = head.next
+    return ret
+
+
+def get_len(head: ListNode) -> int:
+    """
+    head: An object of ListNode type
+
+    Note: Head may be some other Linked List object as long as it uses the .next pointer for the next node
+
+    Warning: Do not pass in any linked list with a loop in it, or the function goes into infinite execution
+    """
+    ret = 0
+    while head:
+        ret += 1
+        head = head.next
+    return ret
+
+
+def get_tail(head: ListNode) -> ListNode:
+    """
+    head: An object of ListNode type
+
+    Note: Head may be some other Linked List object as long as it uses .next and .prev pointers for the next and prev node
+
+    Warning: Do not pass in any linked list with a loop in it, or the function goes into infinite execution
+    """
+    while head.next:
+        head = head.next
+    return head
+
+
+def reverse_ll(head: ListNode, get_tail: bool = False) -> ListNode:
+    """
+    head: An object of ListNode type
+    get_tail: Set true to get the tail of the reversed list as well in the tuple (head, tail)
+
+    Note: Head may be some other Linked List object as long as it uses .next and .prev pointers for the next and prev node
+
+    Warning: Do not pass in any linked list with a loop in it, as that will result in undefined behaviour
+    """
+    # Check if the linked list is linked doubly
+
+    final_tail = head
+
     prev = None
     while head:
+        # Store the actual next aside
         actual_next = head.next
+
+        # Change the next node to whatever the previous node is
         head.next = prev
+
+        # If the linked list is doubly linked, set the prev node to actual_next
+        if hasattr(head, "prev"):
+            head.prev = actual_next
+
         prev = head
         head = actual_next
 
+    if get_tail:
+        return prev, final_tail
     return prev
